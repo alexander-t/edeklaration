86804f999106d2fee6fd3153070662e509a7e0d6
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2018-05-02 22:18:02 +0200

    Added JavaScript boiler plate.

---
+++ js/test/taxcalculator.test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+var taxCalculator = require('../app/taxcalculator');
+
+describe('A Swedish tax calculator', function() {
+    it('Delete this test! It just shows that a dummy function is called.', function () {
+        assert.equal(taxCalculator.ping(), "pong");
+    });
+});
